bc1e492acd00657430cda793af8a5b2b096229f8
kate <leitchka@gmail.com>
2018-05-04 16:30:51 -0700

    Edited skytracker to use the defisheye module on a frame-by-frame basis

--- skytracker/skytracker.py
+++ skytracker/skytracker.py
@@ -8,15 +8,22 @@ import numpy as np
 from median_background import MedianBackground
 from blob_finder import BlobFinder
 
+### KJL 2018_04_18 #### trying to integrate de_fisheye with this workflow
+from defisheye import DeFisheye # now need to make a difisheye.py script in this folder wiht a DeFisheye class in it
 
+
+# DIM=(2592, 1944)
+# K=np.array([[1262.179277672335, 0.0, 1275.466404337509], [0.0, 1261.8632092942269, 1001.5137484034931], [0.0, 0.0, 1.0]])
+# D=np.array([[-0.03450569015228284], [0.003778400552623656], [-0.016075235821126792], [0.009150051563118222]])
+# ########################
 
 class SkyTracker:
 
     default_param = {
             'bg_window_size': 11,
             'fg_threshold': 10,
-            'datetime_mask': {'x': 410, 'y': 20, 'w': 500, 'h': 40}, 
-            'min_area': 0, 
+            'datetime_mask': {'x': 410, 'y': 20, 'w': 500, 'h': 40},
+            'min_area': 0,
             'max_area': 100000,
             'open_kernel_size': (3,3),
             'close_kernel_size': (3,3),
@@ -25,11 +32,15 @@ class SkyTracker:
             'output_video_fps': 20.0,
             'blob_file_name': 'blob_data.json',
             'show_dev_images' : False,
-            'min_interblob_spacing' : 2
-            }
+            'min_interblob_spacing' : 2,
+            'perform_calibration':True,
+            'calibration_checkerboard_internal_corners': (6,9),
+            'defisheye_balance' : 1.0}
 
-    def __init__(self, input_video_name, param=default_param):
+    def __init__(self, input_video_name, checkerboard_video_name, param=default_param):
         self.input_video_name = input_video_name
+        #KJL 2018_04_19
+        self.checkerboard_video_name = checkerboard_video_name
         self.param = self.default_param
         if param is not None:
             self.param.update(param)
@@ -39,13 +50,15 @@ class SkyTracker:
         y = self.param['datetime_mask']['y']
         w = self.param['datetime_mask']['w']
         h = self.param['datetime_mask']['h']
-        img_masked = np.array(img) 
+        img_masked = np.array(img)
         img_masked[y:y+h, x:x+w] = np.zeros([h,w,3])
         return img_masked
 
     def run(self):
 
         cap = cv2.VideoCapture(self.input_video_name)
+        # KJL 2018_04_19
+
 
         bg_model = MedianBackground(
                 window_size=self.param['bg_window_size'],
@@ -53,8 +66,8 @@ class SkyTracker:
                 )
 
         blob_finder = BlobFinder(
-                filter_by_area=True, 
-                min_area=self.param['min_area'], 
+                filter_by_area=True,
+                min_area=self.param['min_area'],
                 max_area=self.param['max_area'],
                 open_kernel_size = self.param['open_kernel_size'],
                 close_kernel_size = self.param['close_kernel_size'],
@@ -62,8 +75,15 @@ class SkyTracker:
 		#---------KJL 2017_12_15
                 min_interblob_spacing = self.param['min_interblob_spacing'])
 
+        # KJL 2018_04_19
+        if self.param['perform_calibration']:
+            defisheye = DeFisheye(checkerboard_num_of_internal_corners = self.param['calibration_checkerboard_internal_corners'],
+                                balance = self.param['defisheye_balance'] )
+        #Here I'm running the fisheye calibration on the checkerboard video for this camera
+            calib_dim, K, D = defisheye.calibrate(checkerboard_video_name = self.checkerboard_video_name)
+
         # Output files
-        vid = None  
+        vid = None
         blob_fid = None
 
         if self.param['blob_file_name'] is not None:
@@ -82,7 +102,12 @@ class SkyTracker:
             frame_count += 1
 
             frame = self.apply_datetime_mask(frame)
+            ##### HERE IS WHERE I THINK defisheye.undistort COULD BE CALLED, ON A FRAME-BY-FRAME BASIS ####
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if self.param['perform_calibration']:
+                frame = defisheye.undistort(img = frame, DIM = calib_dim, K = K, D = D)
+
+            #############################################################
 
             if frame_count == 0 and self.param['output_video_name'] is not None:
                 vid = cv2.VideoWriter(
@@ -99,7 +124,7 @@ class SkyTracker:
                 #        )
 
 
-            # Update background model 
+            # Update background model
             bg_model.update(frame)
             if not bg_model.ready:
                 continue
@@ -111,7 +136,7 @@ class SkyTracker:
                 vid.write(circ_image)
 
             if blob_fid is not None:
-                frame_data = {'frame': frame_count, 'blobs' : blob_list} 
+                frame_data = {'frame': frame_count, 'blobs' : blob_list}
                 frame_data_json = json.dumps(frame_data)
                 blob_fid.write('{0}\n'.format(frame_data_json))
 
@@ -124,11 +149,11 @@ class SkyTracker:
                 cv2.imshow('circ_image', circ_image)
             else:
                 cv2.imshow('circ_image', circ_image)
-                
+
             wait_key_val = cv2.waitKey(1) & 0xFF
             if wait_key_val == ord('q'):
                 break
-            
+
         # Clean up
         cap.release()
         cv2.destroyAllWindows()
@@ -150,12 +175,11 @@ class SkyTracker:
 #    param = {
 #            'bg_window_size': 11,
 #            'fg_threshold': 10,
-#            'datetime_mask': {'x': 430, 'y': 15, 'w': 500, 'h': 40}, 
-#            'min_area': 1, 
+#            'datetime_mask': {'x': 430, 'y': 15, 'w': 500, 'h': 40},
+#            'min_area': 1,
 #            'max_area': 100000,
 #            'open_kernel_size': (3,3),
 #            }
 #
 #    tracker = SkyTracker(input_video_name=input_video_name, param=param)
 #    tracker.run()
-
